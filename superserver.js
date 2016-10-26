36a4f9fabd68181b8869f7107909fc93bb5498af
Juanita Soranno <juanitasoranno@gmail.com>
2016-10-25 23:39:55 -0400

    additions

--- superserver.js
+++ superserver.js
@@ -6,21 +6,22 @@ var PORT2 = 7500;
 
 
 function handleRequest(request, response){
-    if (request == PORT1) {
+    console.log(request);
         response.end("You are so intelligent. I always learn something new when I see you.");
-    }if (request == PORT2){
-        response.end("Please take a shower and brush your teeth, smelly cat.");
-    }
 };
-var server = http.createServer(handleRequest);
+function handleRequest2(request, response){
+        response.end("Please take a shower and brush your teeth, smelly cat.");
+};
+var server1 = http.createServer(handleRequest);
+var server2 = http.createServer(handleRequest2);
 
 // Listener (starts our server)
-server.listen(PORT1, function(){
+server1.listen(PORT1, function(){
 
     console.log("I am now listening at PORT: " + PORT1);
 
 })
-server.listen(PORT2, function(){
+server2.listen(PORT2, function(){
 
     console.log("I am now listening at PORT: " + PORT2);
 
